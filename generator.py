02b854b7bc0689f86d8f57a9682519df822e7e72
shoogar7 <134614243+shoogar7@users.noreply.github.com>
2026-02-16 19:51:49 +0100

    delete file format check; add requirements.txt; add Pillow functionalities

--- generator.py
+++ generator.py
@@ -1,13 +1,29 @@
+from PIL import Image, ImageOps
+
 def load_image():
     # loading file_name of image from predefined variable, could later add console or UI
     file_name = "image.jpg"
     # file_name = str(input("Provide name of the image file: "))
 
-    # checking file_name extention, could later check from a list or add a variable
-    if file_name[-1:-5:-1] != "gnp." and file_name[-1:-5:-1] != "gpj.":
-        print("Wrong file format")
-    else:
-        print(f"Generating ASCII from {file_name}")
+    # create Image object with Pillow
+    with Image.open(file_name) as image:
+            
+        # loading output size of image from predefined variable, could later add console or UI
+        ascii_width = 50
+        # we assume the image is vertical and calculate height (can add automatic detection later)
+        ascii_height = ascii_width * 2
+
+        # resizing the image
+        image = image.resize((ascii_width, ascii_height))
+
+        # testing another options for resizing the image
+        # image = ImageOps.fit(image, (ascii_width, ascii_height))
+
+        # to make it easier to translate into ASCII, we convert the image to black-white
+        image = image.convert("L")
+
+        # testing how the image looks for now
+        # image.show()
 
 def main():
     load_image()
