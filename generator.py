e3f77920be474643218b8c2167c12ddea22e5563
shoogar7 <134614243+shoogar7@users.noreply.github.com>
2026-02-18 01:52:07 +0100

    clean up the logic in generate_art()

--- generator.py
+++ generator.py
@@ -3,50 +3,25 @@ from PIL import Image, ImageOps
 # data - from which generate the art
 # width - to be sure when the line ends
 def generate_art(data, ascii_width):
-
-    # table of building blocks
-    # ----------------------
-    # # @ % = + * -   
-    # ----------------------
-    # 0-36 #
-    # 37-72 @
-    # 73-109 %
-    # 110-145 =
-    # 146-182 +
-    # 183-218 *
-    # 219-255 -
-    # ----------------------
-
     with open("ascii.txt", "w") as file:
-            # defining placeholders for line and file contents
-            line = ""
-            content = ""
+            content = "" # placeholder 
 
-            # turning every pixel into ASCII sign
-            for i, pixel in enumerate(data):
-                if (pixel>=0 and pixel<=36):
-                    line+="#"
-                elif (pixel>=37 and pixel<=72):
-                    line+="@"
-                elif (pixel>=73 and pixel<=109):
-                   line+="%"
-                elif (pixel>=110 and pixel<=145):
-                    line+="="
-                elif (pixel>=146 and pixel<=182):
-                    line+="+"
-                elif (pixel>=183 and pixel<=218):
-                    line+="*"
-                elif (pixel>=219 and pixel<=255):
-                    line+="-"
+            ASCII_chars = ("#", "@", "%", "=", "+", "*", "-")
+            ASCII_count = len(ASCII_chars)
+            MAX_LIGHT_VAL = 256
+            
+            # calculate ascii index for every pixel
+            for light_val in data:
+                content += ASCII_chars[(light_val * ASCII_count) // MAX_LIGHT_VAL]
 
-                # keeping track of every line
-                if i % ascii_width == 0:
-                    line+="\n"
-                    content+=line
-                    # clearing the line holder to make space for the new one
-                    line = ""
-            # saving the art to designated file
-            file.write(content)
+            final_content = "" # storing data before saving to file
+
+            # slice the data into rows and store 
+            for i in range(0, len(data), ascii_width):
+                 final_content += content[i:i+ascii_width] + "\n"
+                
+            # # saving the art to designated file
+            file.write(final_content)
 
 def load_image():
     # loading file_name of image from predefined variable, could later add console or UI
@@ -57,12 +32,13 @@ def load_image():
     with Image.open(file_name) as image:
             
         # loading output size of image from predefined variable, could later add console or UI
-        ascii_width = 50
+        ascii_width = 150
         # we assume the image is vertical and calculate height (can add automatic detection later)
-        ascii_height = (int)(ascii_width * 1.5)
+        # parsing to int because float crashes
+        ascii_height = (int)(ascii_width * 1.05)
 
         # resizing the image
-        image.thumbnail((ascii_width, ascii_height))
+        image = image.resize((ascii_width, ascii_height))
 
         # testing another options for resizing the image
         # image = ImageOps.fit(image, (ascii_width, ascii_height))
